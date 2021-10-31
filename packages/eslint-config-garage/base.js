5bb7b67045a139b1396b50bb246e4d9ccc0239b1
Sergey Ushakov <sergey@ushakov.io>
2021-10-31 17:32:20 +0200

    prettierfied base.js

--- packages/eslint-config-garage/base.js
+++ packages/eslint-config-garage/base.js
@@ -5,39 +5,35 @@ module.exports = {
     node: true,
   },
   extends: [
-    "airbnb-base",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "unused-imports"],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-vars": [
-      "error",
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-vars': [
+      'error',
       {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
         ignoreRestSiblings: true,
       },
     ],
-    "unused-imports/no-unused-imports": "error",
-    "import/order": [
-      "error",
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
       {
-        groups: [
-          ["builtin", "external"],
-          "internal",
-          ["parent", "sibling", "index"],
-        ],
-        "newlines-between": "always",
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
       },
     ],
   },
