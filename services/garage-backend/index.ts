19dd7abfd257771093a4cc7976249a54d33613f3
Sergey Ushakov <sergey@ushakov.io>
2021-10-31 17:31:07 +0200

    added prettier

--- services/garage-backend/index.ts
+++ services/garage-backend/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import fs from "fs";
+import express from 'express';
+import fs from 'fs';
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
