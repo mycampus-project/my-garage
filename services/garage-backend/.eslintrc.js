215bf8fda3a1733c6d62ecf3094fe9185e33dfb6
Sergey Ushakov <sergey@ushakov.io>
2021-11-01 07:43:25 +0200

    set up garage-backend to use common

--- services/garage-backend/.eslintrc.js
+++ services/garage-backend/.eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   extends: ['eslint-config-garage'],
   parserOptions: {
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: '.',
     project: './tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
   },
 };
