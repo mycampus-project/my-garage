c890f71ebd9cfbe54f0964922ec583ac75ae0eea
Michael Carter <michael_carter_21@hotmail.com>
2021-11-03 23:10:03 +0200

    checking that admin tests only runs on admin changes

--- apps/garage-timebooking/src/App.tsx
+++ apps/garage-timebooking/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload. Timebooking from Nokia repo finally
-          working!
+          working! This shouldnt start tests.
         </p>
         <a
           className="App-link"
