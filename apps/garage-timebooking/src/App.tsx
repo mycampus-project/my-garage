7ad73734c751047e7c36c7124fa8d84a932e5ff7
Michael Carter <michael_carter_21@hotmail.com>
2021-11-03 20:59:44 +0200

    this action wont launch

--- apps/garage-timebooking/src/App.tsx
+++ apps/garage-timebooking/src/App.tsx
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Timebooking from Nokia repo!
+          Edit <code>src/App.tsx</code> and save to reload. Timebooking from Nokia repo finally
+          working!
         </p>
         <a
           className="App-link"
