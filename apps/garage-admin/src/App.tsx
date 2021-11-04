d3680ecb03e3922c9d9866bde0dc81b96160ceed
Michael Carter <michael_carter_21@hotmail.com>
2021-11-04 10:03:23 +0200

    running test after fixing bad code

--- apps/garage-admin/src/App.tsx
+++ apps/garage-admin/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing failure
+          Learn React
         </a>
       </header>
     </div>
