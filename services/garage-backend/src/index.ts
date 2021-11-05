a5c335bafb74d1a833a0eadd729367d2683a8ba9
Sergey Ushakov <sergey@ushakov.io>
2021-11-05 16:25:51 +0200

    re-enable mongoose connections

--- services/garage-backend/src/index.ts
+++ services/garage-backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-// import mongo from './mongo';
+import mongo from './mongo';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -13,6 +13,7 @@ const listen = () => {
   });
 };
 
-listen();
-console.log(process.env.MONGODB_URL);
-// mongo.once('open', () => console.log('Mongo connection open'));
+mongo.once('open', () => {
+  console.log('Mongo connection open');
+  listen();
+});
