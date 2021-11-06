923a3283ce46c285458208a5cf6eb3d290fae0ed
Sergey Ushakov <sergey@ushakov.io>
2021-11-06 11:46:13 +0200

    setup child pages in admin

---
+++ apps/garage-admin/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+
+function Login() {
+  return (
+    <Layout>
+      <Content>Login</Content>
+    </Layout>
+  );
+}
+
+export default Login;
