7ffd14ee6f352f7c849feef1058e33d0518eb70e
Sergey Ushakov <sergey@ushakov.io>
2021-11-06 12:18:50 +0200

    rename test

--- apps/garage-admin/src/App.test.tsx
+++ apps/garage-admin/src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders global styles', async () => {
+test('renders side menu', async () => {
   render(<App />);
 
   await waitFor(() => {
