e1538f9ac4166be79b7346ad4587dd0b3d190b97
Sergey Ushakov <sergey@ushakov.io>
2021-11-06 16:57:33 +0200

    add bcrypt_secret and jwtSecret to backend config

--- services/garage-backend/src/config.ts
+++ services/garage-backend/src/config.ts
@@ -1,4 +1,8 @@
-const config = { db: process.env.MONGODB_URL };
+const config = {
+  db: process.env.MONGODB_URL,
+  bcryptSecret: process.env.BCRYPT_SECRET,
+  jwtSecret: process.env.JWT_SECRET,
+};
 Object.freeze(config);
 
 export default config as Readonly<typeof config>;
