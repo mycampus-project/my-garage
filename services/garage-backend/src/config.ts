c4f3deeb35b80f0cd0a8dab8d171c28c1a2a182d
Sergey Ushakov <sergey@ushakov.io>
2021-11-07 13:02:59 +0200

    added utils for encrypting and decrypting things

--- services/garage-backend/src/config.ts
+++ services/garage-backend/src/config.ts
@@ -5,10 +5,14 @@ dotenv.config();
 const config = {
   db: process.env.MONGODB_URL,
   jwtSecret: process.env.JWT_SECRET!,
+  cryptoSecret: process.env.CRYPTO_SECRET!,
 };
 
 if (!config.jwtSecret) {
   throw Error('Please supply JWT_SECRET environment variable');
+}
+if (!config.cryptoSecret) {
+  throw Error('Please supply CRYPTO_SECRET environment variable');
 }
 
 Object.freeze(config);
