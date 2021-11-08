0163ed795f0e50a0d73b830161e1a1034c34f50e
Ashif Iqbal <bappys@hotmail.com>
2021-11-08 10:01:44 +0200

    looger and api error handler added

--- services/garage-backend/src/middlewares/apiErrorHandler.ts
+++ services/garage-backend/src/middlewares/apiErrorHandler.ts
@@ -1,1 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
 
+import ApiError from '../helpers/apiError';
+import logger from '../util/logger';
+
+// eslint-disable-next-line func-names
+export default function (error: ApiError, req: Request, res: Response, next: NextFunction) {
+  if (error.source) {
+    logger.error(error.source);
+  }
+
+  res.status(error.statusCode).json({
+    status: 'error',
+    statusCode: error.statusCode,
+    message: error.message,
+  });
+}
