036cc65aa577550e914ed89e61d086e00a85d669
Sergey Ushakov <sergey@ushakov.io>
2021-11-15 20:27:18 +0200

    set up ant design theming in timebooking

--- apps/garage-timebooking/src/App.tsx
+++ apps/garage-timebooking/src/App.tsx
@@ -1,18 +1,30 @@
-import './App.css';
-import 'antd/dist/antd.css';
-
 import { createGlobalStyle } from 'styled-components';
+import { ConfigProvider } from 'antd';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import RouterRoot from './RouterRoot';
-import AuthContextProvider from './contexts/AuthContext';
+import 'antd/dist/antd.variable.min.css';
 
-const client = new QueryClient();
+import AuthContextProvider from './contexts/AuthContext';
 
 const GlobalStyles = createGlobalStyle`
   #root {
     height: 100%;
+    --padding-xs: 4px;
+    --padding-s: 8px;
+    --padding-m: 16px;
+    --padding-l: 24px;
+    --padding-xl: 32px;
   }
 `;
+
+// Define theme variables for ant-design
+ConfigProvider.config({
+  theme: {
+    primaryColor: '#0065ED',
+  },
+});
+
+const client = new QueryClient();
 
 function App() {
   return (
