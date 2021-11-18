7b9b56e817b5a09f7a1db856f41e6c207bb1ce7e
Michael Carter <michael_carter_21@hotmail.com>
2021-11-18 02:22:23 +0200

    add device form made

--- apps/garage-admin/src/components/admin/Devices/AddDeviceModel.tsx
+++ apps/garage-admin/src/components/admin/Devices/AddDeviceModel.tsx
@@ -1,21 +1,53 @@
 import { useContext } from 'react';
-import { Modal } from 'antd';
+import { Modal, Form, Input, Switch, Select } from 'antd';
 import { AdminContext } from 'src/contexts/AdminContext';
+
+const { Option } = Select;
+const { TextArea } = Input;
 
 const AddDeviceModel = () => {
   const { addDeviceIsVisible, setAddDeviceIsVisible } = useContext(AdminContext);
+  const [form] = Form.useForm();
+
+  const onChange = () => {};
+
+  const handleChange = () => {};
+
   return (
     <Modal
-      title="Model for adding new Devices"
+      title="Add New Device"
       centered
       visible={addDeviceIsVisible}
-      onOk={() => setAddDeviceIsVisible(false)}
+      onOk={() => {
+        form.submit();
+        setAddDeviceIsVisible(false);
+      }}
       onCancel={() => setAddDeviceIsVisible(false)}
-      width={1000}
+      width={500}
     >
-      <p>some contents...</p>
-      <p>some contents...</p>
-      <p>some contents...</p>
+      <Form form={form} layout="vertical" name="userForm">
+        <Form.Item name="name" label="Device Name" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="Description" rules={[{ required: true }]}>
+          <TextArea rows={4} />
+        </Form.Item>
+        <Form.Item name="type" label="Type" rules={[{ required: true }]}>
+          <Select defaultValue="-" style={{ width: 120 }} onChange={handleChange}>
+            <Option value="Room">Room</Option>
+            <Option value="Printer">Printer</Option>
+            <Option value="Peripheral">Peripheral</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="isAvailable" label="Availability" rules={[{ required: true }]}>
+          <Switch
+            checkedChildren="Available"
+            unCheckedChildren="Unavailable"
+            defaultChecked
+            onChange={onChange}
+          />
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
