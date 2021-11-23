2cbaeceb0bb1fe59a38097dc70a18fd25d1127e1
Sergey Ushakov <sergey@ushakov.io>
2021-11-23 21:26:34 +0200

    refactor the shit out of device booking

--- apps/garage-timebooking/src/components/NewBooking/TimeCell.tsx
+++ apps/garage-timebooking/src/components/NewBooking/TimeCell.tsx
@@ -1,7 +1,11 @@
 import styled, { css } from 'styled-components';
 import { CELL_WIDTH, TableCell } from './common';
 
-const TimeTableCell = styled(TableCell)<{ isHighlighted: boolean }>`
+const TimeTableCell = styled(TableCell)<{
+  isHighlighted: boolean;
+  isUnavalilable: boolean;
+  isSelected: boolean;
+}>`
   flex: 1;
 
   &:nth-child(2n + 1) {
@@ -17,9 +21,30 @@ const TimeTableCell = styled(TableCell)<{ isHighlighted: boolean }>`
     css`
       &,
       &:hover {
+        background-color: var(--ant-primary-4) !important;
+        color: black;
+        border-color: transparent;
+      }
+    `};
+
+  ${({ isSelected }) =>
+    isSelected &&
+    css`
+      &,
+      &:hover {
         background-color: var(--ant-primary-color) !important;
         color: white;
         border-color: transparent;
+      }
+    `};
+
+  ${({ isUnavalilable }) =>
+    isUnavalilable &&
+    css`
+      & {
+        background-color: #c8c8c8 !important;
+        border-color: transparent;
+        cursor: not-allowed;
       }
     `};
 
@@ -31,6 +56,8 @@ const TimeTableCell = styled(TableCell)<{ isHighlighted: boolean }>`
 interface Props {
   date: Date;
   isHighlighted: boolean;
+  isUnavailable: boolean;
+  isSelected: boolean;
   onMouseEnter: (date: Date) => void;
   onMouseLeave: (date: Date) => void;
   onClick: (date: Date) => void;
@@ -39,14 +66,18 @@ interface Props {
 const TimeCell: React.FC<Props> = ({
   date,
   isHighlighted,
+  isUnavailable,
+  isSelected,
   onClick,
   onMouseEnter,
   onMouseLeave,
   children,
 }) => (
   <TimeTableCell
-    onClick={() => onClick(date)}
+    onClick={() => !isUnavailable && onClick(date)}
     isHighlighted={isHighlighted}
+    isUnavalilable={isUnavailable}
+    isSelected={isSelected}
     onMouseEnter={() => onMouseEnter(date)}
     onMouseLeave={() => onMouseLeave(date)}
   >
