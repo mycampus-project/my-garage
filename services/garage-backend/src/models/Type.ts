6a9c3fc99008b02fac3fc87b6d2c56140816d86b
Ashif Iqbal <bappys@hotmail.com>
2021-11-24 16:09:50 +0200

    Type model created

---
+++ services/garage-backend/src/models/Type.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document, ObjectId } from 'mongoose';
+import { Type } from '@my-garage/common';
+
+export interface TypeDocument
+  extends Document<ObjectId>,
+    Omit<Type, 'createdBy' | 'removedBy' | 'id'> {
+  createdBy: ObjectId;
+  removedBy?: ObjectId;
+}
+
+const typeSchema = new mongoose.Schema<TypeDocument>({
+  name: {
+    type: String,
+    index: { unique: true },
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: () => new Date(),
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  removedAt: {
+    type: Date,
+    required: false,
+  },
+  removedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+export default mongoose.model('Type', typeSchema);
