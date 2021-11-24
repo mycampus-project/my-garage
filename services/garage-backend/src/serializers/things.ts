b506808974f57f56d95b85b3a458b174418ac8dc
Ashif Iqbal <bappys@hotmail.com>
2021-11-24 19:53:18 +0200

    update in thing serialization

--- services/garage-backend/src/serializers/things.ts
+++ services/garage-backend/src/serializers/things.ts
@@ -2,10 +2,11 @@ import { Thing } from '@my-garage/common';
 
 import User, { UserDocument } from '../models/User';
 import { ThingDocument } from '../models/Thing';
+import Type, { TypeDocument } from '../models/Type';
 
 // eslint-disable-next-line import/prefer-default-export
 export const serializeThing = async (thing: ThingDocument): Promise<Thing> => {
-  const { _id: id, name, description, type, isAvailable, createdAt, removedAt, imageUrl } = thing;
+  const { _id: id, name, description, isAvailable, createdAt, removedAt, imageUrl } = thing;
   const createThingWithUser = await thing.populate<{ createdBy: UserDocument }>({
     path: 'createdBy',
     model: User,
@@ -14,12 +15,16 @@ export const serializeThing = async (thing: ThingDocument): Promise<Thing> => {
     path: 'removedBy',
     model: User,
   });
+  const createThingWithType = await thing.populate<{ type: TypeDocument | null }>({
+    path: 'type',
+    model: Type,
+  });
 
   return {
     id: id!.toString(),
     name,
     description,
-    type,
+    type: createThingWithType.type.name,
     isAvailable,
     createdAt,
     createdBy: {
