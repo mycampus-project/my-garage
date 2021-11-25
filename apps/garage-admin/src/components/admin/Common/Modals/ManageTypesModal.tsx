3d1ac123d2701172680abbaea3ca3d0ae584bd88
Michael Carter <michael_carter_21@hotmail.com>
2021-11-25 12:55:53 +0200

    type functionality added

--- apps/garage-admin/src/components/admin/Common/Modals/ManageTypesModal.tsx
+++ apps/garage-admin/src/components/admin/Common/Modals/ManageTypesModal.tsx
@@ -1,5 +1,7 @@
-import { Button, Modal, Space } from 'antd';
-import { useContext, useState } from 'react';
+import { Type } from '@my-garage/common';
+import { Button, Modal } from 'antd';
+import { useContext, useEffect, useState } from 'react';
+import useType from 'src/hooks/useType';
 import styled from 'styled-components';
 import { AdminContext } from '../../../../contexts/AdminContext';
 import TypeList from '../../Devices/TypeList';
@@ -13,6 +15,35 @@ const ManageTypesModal = () => {
   const { modelIsVisible, setModelIsVisible } = useContext(AdminContext);
   const [toggleRestoreTypes, setToggleRestoreTypes] = useState<boolean>(false);
 
+  const { data, error } = useType().GetListOfTypes();
+  const [filteredData, setFilteredData] = useState<Type[]>([]);
+
+  const sortedArray = (dataArray: Type[]) => {
+    function compareByName(a: Type, b: Type) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    }
+
+    return dataArray.sort(compareByName);
+  };
+
+  useEffect(() => {
+    const sortedData = data ? sortedArray(data.data) : new Array<Type>();
+    const filteredArray = toggleRestoreTypes
+      ? sortedData.filter((item: Type) => item.removedBy !== undefined)
+      : sortedData.filter((item: Type) => item.removedBy === undefined);
+    setFilteredData(filteredArray);
+  }, [data, toggleRestoreTypes]);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
   return (
     <Modal
       title="Manage Types"
@@ -21,6 +52,7 @@ const ManageTypesModal = () => {
       onCancel={() => setModelIsVisible(false)}
       footer={[
         <StyledButton
+          key={1}
           type="primary"
           onClick={() => {
             setToggleRestoreTypes(!toggleRestoreTypes);
@@ -30,14 +62,8 @@ const ManageTypesModal = () => {
         </StyledButton>,
       ]}
     >
-      {toggleRestoreTypes ? (
-        <TypeList showRestoreData />
-      ) : (
-        <Space direction="vertical" size={56} style={{ width: '100%' }}>
-          <TypeList />
-          <AddTypeForm />
-        </Space>
-      )}
+      <TypeList data={filteredData} showRestore={toggleRestoreTypes} />
+      {!toggleRestoreTypes && <AddTypeForm />}
     </Modal>
   );
 };
