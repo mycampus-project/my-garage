4b9d317098c658a61fa17ad9d16d564f199d53c3
Sergey Ushakov <sergey@ushakov.io>
2021-11-27 16:09:16 +0200

    adjusted size of table cells

--- apps/garage-timebooking/src/components/NewBooking/common.tsx
+++ apps/garage-timebooking/src/components/NewBooking/common.tsx
@@ -4,8 +4,8 @@ export const CELL_WIDTH = 200;
 
 export const TableCell = styled.td`
   text-align: center;
-  line-height: 1.1;
+  line-height: 1;
   background: white;
-  height: 19px;
+  height: 16px;
   width: 1px; // Stops cell from growing in width
 `;
