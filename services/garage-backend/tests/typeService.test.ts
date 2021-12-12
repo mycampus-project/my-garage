3ccbbb8816feabd9e7ef4132d0f08721195c5b38
Ashif Iqbal <bappys@hotmail.com>
2021-12-12 21:58:12 +0200

    type service test done

--- services/garage-backend/tests/typeService.test.ts
+++ services/garage-backend/tests/typeService.test.ts
@@ -9,6 +9,7 @@ async function createType() {
   const type = new Type({
     name: 'Compute',
     maxBookingDuration: 120,
+    createdBy: randomObjectId,
   });
   return await TypeService.createType(type);
 }
@@ -31,5 +32,49 @@ describe('type service', () => {
     expect(thing).toHaveProperty('_id');
     expect(thing).toHaveProperty('name', 'Compute');
     expect(thing).toHaveProperty('maxBookingDuration', 120);
+  });
+
+  it('should get a type with id', async () => {
+    const type = await createType();
+    const found = await TypeService.findTypeById(type.id);
+    expect(found.name).toEqual(type.name);
+    expect(found.maxBookingDuration).toEqual(type.maxBookingDuration);
+  });
+
+  it('should not get a non existing type', async () => {
+    expect.assertions(1);
+    return TypeService.findTypeById(nonExistingTypeId).catch((e) => {
+      expect(e.message).toMatch(`Type ${nonExistingTypeId} not found`);
+    });
+  });
+
+  it('should update an existing type', async () => {
+    const type = await createType();
+    const update = {
+      name: 'Storage',
+      maxBookingDuration: 60,
+    };
+    const updatedType = await TypeService.updateType(type.id, update);
+    expect(updatedType).toHaveProperty('id', type.id);
+    expect(updatedType).toHaveProperty('name', 'Storage');
+    expect(updatedType).toHaveProperty('maxBookingDuration', 60);
+  });
+
+  it('should not update a non-existing type', async () => {
+    expect.assertions(1);
+    const update = {
+      name: 'Storage',
+      maxBookingDuration: 60,
+    };
+    return TypeService.updateType(nonExistingTypeId, update).catch((e) => {
+      expect(e.message).toMatch(`Type ${nonExistingTypeId} not found`);
+    });
+  });
+
+  it('should remove an existing type', async () => {
+    const type = await createType();
+    const removedType = await TypeService.deleteType(type.id, randomObjectId, new Date());
+    expect(removedType).toHaveProperty('removedBy');
+    expect(removedType).toHaveProperty('removedAt');
   });
 });
