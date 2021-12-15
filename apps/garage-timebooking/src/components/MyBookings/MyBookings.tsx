76a51a0dbcb462b02d212b98d9f0258f0489c31f
Sergey Ushakov <sergey@ushakov.io>
2021-12-15 16:45:06 +0200

    implemented ui for previous user

--- apps/garage-timebooking/src/components/MyBookings/MyBookings.tsx
+++ apps/garage-timebooking/src/components/MyBookings/MyBookings.tsx
@@ -69,6 +69,7 @@ const useMyBookings = () => {
           userId: user.id,
           limit: 0,
           mode: 'future',
+          includePrevious: true,
         },
       })
       .then((response) => response.data)
